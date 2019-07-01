2fd6c2d15cc7864181c04d5e2b2e36548dc17300
Sara Grimes <sarajoy415@gmail.com>
2019-07-01 02:47:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,56 @@
+function add(a, b){
+ var a = Math.floor(Math.random() * 1000)
+ varb = Math.floor(Math.random() * 1000)
+  return
+}
+function subtract(a, b){
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)  
+  return
+
+}
+function multiply(a, b){
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000) 
+  return
+}
+function divide(a, b){
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000) 
+  
+  return
+}
+function increment(a, b){
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)  
+  return
+}
+function decrement(a--b){
+var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)  
+  return
+}
+function int(parseInt(a) + parseInt(b){
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000) 
+  return
+}
+function base( b){
+ var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)  
+  return
+}
+
+function nan(, b){
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000)  
+  return
+  
+function preserve(, b){
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)
+  return
+function nan(, b){
+ var a = Math.floor(Math.random() * 1000)
+ var b = Math.floor(Math.random() * 1000) 
+  return
