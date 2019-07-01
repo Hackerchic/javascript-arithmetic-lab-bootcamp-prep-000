6b5564a5db8b28feb6fdc19e2d45453d751c5bfe
Sara Grimes <sarajoy415@gmail.com>
2019-07-01 03:26:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function divide(a, b){
 return a/b
 }
 function inc(n){
-return n + 1
+return n + 1}
 function dec(n){
-return n - 1
+return n - 1}
 function makeInt(n){
 return parseInt(n,10)}
   
