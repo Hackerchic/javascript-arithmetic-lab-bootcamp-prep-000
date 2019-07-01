683e1eb8d2d522a5c99a5b8c110dc621cfd2f5c8
Sara Grimes <sarajoy415@gmail.com>
2019-07-01 03:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ return a*b
 function divide(a, b){
 return a/b
 }
-function inc(a){
-return a++
+function inc(n){
+return n++
 }
-function dec(a){
-return a--
+function dec(n){
+return n--
 }
 function makeInt(n){
-return parseInt(n)}
+return parseInt(n,10)}
   
 function preserveDecimal(n){
 return parseFloat(n)}
